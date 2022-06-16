b167bca102c4fb39006a4daa67f1ca21899062ec
longdang97777777 <86861334+longdang97777777@users.noreply.github.com>
2022-06-16 20:57:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Xù à!',
+    introTitle: 'Như à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -12,8 +12,8 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Nhớ live thường xuyên nhé <3<3<3.',
+    mess: 'Anh biết mà 🥰 😘😘',
+    messDesc: 'Hết dỗi anh nhé <3<3<3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/dinhnam246' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: '' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
